25ebd57da8577409f97438f490c9b4553797d365
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-11-17 08:58:47 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button>hey</Button>
+      hello
+    </div>
+  );
+}
