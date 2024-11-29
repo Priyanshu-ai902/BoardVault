e68153b412107d191a10246dd860751913548ea9
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-11-29 19:34:15 +0530

    home is added

--- components/header.jsx
+++ components/header.jsx
@@ -5,12 +5,14 @@ import { PlusCircleIcon } from "lucide-react"
 
 const Header = () => {
   return (
-    <header className="container mx-auto">
-      <nav className="py-4 px-3 flex justify-between items-center">
+    <header className="container mx-auto ">
+      <nav className="py-5 px-2 flex justify-between items-center ">
         <Link href="/">
-          <div className="text-2xl font-bold">
-            <span className="text-purple-400 dark:text-purple-700">Board</span>
-            <span className="text-cyan-400 dark:text-cyan-300">Vault</span>
+          <div className="text-3xl font-bold ">
+            
+              <span className="text-purple-400 dark:text-purple-700">Board</span>
+              <span className="text-cyan-400 dark:text-cyan-300">Vault</span>
+            
           </div>
         </Link>
 
@@ -22,20 +24,20 @@ const Header = () => {
         <div className="flex items-center gap-4">
           <Link href='/project/create'>
             <Button className="bg-purple-600 hover:bg-purple-700 text-white">
-            
-              <PlusCircleIcon size={18}/>
+
+              <PlusCircleIcon size={18} />
               <span>Create Project</span>
             </Button>
           </Link>
 
           <SignedOut>
             <SignInButton forceRedirectUrl="/onboarding" >
-            <Button className="bg-gray-800 hover:bg-gray-900 text-white">Login</Button>
+              <Button className="bg-gray-800 hover:bg-gray-900 text-white">Login</Button>
             </SignInButton>
           </SignedOut>
 
           <SignedIn>
-            
+
           </SignedIn>
         </div>
       </nav>
