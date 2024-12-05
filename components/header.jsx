27555a770205fcecd5dcbaf549270941b8928c90
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-12-05 19:45:50 +0530

    changes

--- components/header.jsx
+++ components/header.jsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button"
 import { PlusCircleIcon } from "lucide-react"
 import UserMenu from "./user-menu"
 import { checkUser } from "@/lib/checkUser"
+import UserLoading from "./user-loading"
 
 const Header = async () => {
 
@@ -45,6 +46,7 @@ const Header = async () => {
           </SignedIn>
         </div>
       </nav>
+      <UserLoading/>
     </header>
   )
 }
--- middleware.js
+++ middleware.js
@@ -12,6 +12,15 @@ export default clerkMiddleware((auth, req) => {
 
     return auth().redirectToSignIn();
   }
+
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/onboarding" &&
+    req.nextUrl.pathname !== "/"
+  ) {
+    return NextResponse.redirect(new URL("/onboarding", req.url));
+  }
 });
 
 export const config = {
