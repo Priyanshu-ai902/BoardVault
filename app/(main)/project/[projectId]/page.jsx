c83e2c739190ef7c8f298f6248c9071944cbe574
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-12-26 18:33:40 +0530

    sprint creation is added

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,6 +1,7 @@
 import { getProject } from "@/actions/projects";
 import { notFound } from "next/navigation";
-
+import SprintCreationForm from "../_components/create-sprint";
+// import SprintBoard from "../_components/sprint-board";
 
 export default async function ProjectPage({ params }) {
   const { projectId } = params;
@@ -12,9 +13,22 @@ export default async function ProjectPage({ params }) {
 
   return (
     <div className="container mx-auto">
-      {/* <SprintCreationForm/> */}
+      <SprintCreationForm
+        projectTitle={project.name}
+        projectId={projectId}
+        projectKey={project.key}
+        sprintKey={project.sprints?.length + 1}
+      />
 
-      {/* {project.sprints} */}
+      {/* {project.sprints.length > 0 ? (
+        <SprintBoard
+          sprints={project.sprints}
+          projectId={projectId}
+          orgId={project.organizationId}
+        />
+      ) : (
+        <div>Create a Sprint from button above</div>
+      )} */}
     </div>
   );
 }
